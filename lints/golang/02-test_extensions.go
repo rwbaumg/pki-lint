ff3dc949c8691d4c88a0ebf508134b8084e8a407
Robert W. Baumgartner <rwb@0x19e.net>
2019-04-27 23:42:44 -0500

    Refactor 02-test_extensions.go to scan entire chain for unhandled extensions.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- lints/golang/02-test_extensions.go
+++ lints/golang/02-test_extensions.go
@@ -58,6 +58,69 @@ func loadPemChain(chainInput string) tls.Certificate {
   return cert
 }
 
+func checkCertificate(cert *x509.Certificate) {
+  printDebug(2, "Checking certificate: %s", cert.Subject.CommonName)
+
+  if len(cert.UnhandledCriticalExtensions) > 0 {
+    fmt.Printf("Go: Found unhandled critical extension(s) in '%s':\n", cert.Subject.CommonName)
+    for _, ext := range cert.UnhandledCriticalExtensions {
+      fmt.Printf("\t   Extension OID: %s\n", ext.String());
+     }
+     os.Exit(1)
+  }
+  return
+}
+
+func getVerifyOpts(chain string, dns_name string, purpose x509.ExtKeyUsage) x509.VerifyOptions {
+  // construct verification options
+  opts := x509.VerifyOptions{
+    DNSName: dns_name,
+    KeyUsages: []x509.ExtKeyUsage{purpose}}
+
+  if len(chain) > 0 {
+    var chainCerts []*x509.Certificate
+
+    // load all PEM-encoded certificates from the provided chain file
+    certChain := loadPemChain(chain)
+    printDebug(2, "Using PEM chain: %s", chain)
+
+     // construct an array containing all chain certificates
+    for _, cert := range certChain.Certificate {
+      x509Cert, err := x509.ParseCertificate(cert)
+      if err != nil {
+        panic(err)
+      }
+      chainCerts = append(chainCerts, x509Cert)
+    }
+
+    if len(chainCerts) > 0 {
+      // initialize certificate variables
+      opts.Roots = x509.NewCertPool()
+      opts.Intermediates = x509.NewCertPool()
+
+      // parse chain certificates, assuming that the first in the array is the root
+      // add the root certificate first
+      var rootCert *x509.Certificate = chainCerts[len(chainCerts)-1]
+      opts.Roots.AddCert(rootCert)
+      printDebug(2, "Added Root CA: %s", rootCert.Subject)
+
+      checkCertificate(rootCert)
+
+      // add remaining certificates as intermediates
+      var intCA *x509.Certificate
+      for i := 1; i < len(chainCerts)-1; i = i + 1 {
+        intCA = chainCerts[i]
+        opts.Intermediates.AddCert(intCA)
+        printDebug(2, "Added Intermediate CA: %s", intCA.Subject)
+
+        checkCertificate(intCA)
+      }
+    }
+  }
+
+  return opts
+}
+
 func main() {
   // Read and parse the PEM certificate file
   if len(os.Args) < 2 {
@@ -68,7 +131,6 @@ func main() {
   var file string
   file = os.Args[1]
 
-  /*
   var chain string
   if len(os.Args) >= 3 {
     chain = os.Args[2]
@@ -88,7 +150,6 @@ func main() {
       printDebug(2, "Checking DNS Name: %s", dns_name)
     }
   }
-  */
 
   pemData, err := ioutil.ReadFile(file)
   if err != nil {
@@ -105,13 +166,11 @@ func main() {
     log.Fatal(err)
   }
 
-  if len(cert.UnhandledCriticalExtensions) > 0 {
-    fmt.Printf("Go: Found unhandled critical extensions:\n")
-    for _, ext := range cert.UnhandledCriticalExtensions {
-      fmt.Printf("    Extension OID: %s\n", ext.String());
-     }
-     os.Exit(1)
-  }
+  // use getVerifyOpts to test certificate chain
+  getVerifyOpts(chain, dns_name, purpose)
+
+  // check the target certificate
+  checkCertificate(cert)
 
   fmt.Printf("Go: No unhandled critical extensions.\n")
   os.Exit(0)
